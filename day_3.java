3c90998d577a2d8dd60cb63aa124e2973340a405
ritesh <riteshraj.developer@gmail.com>
2025-02-03 22:30:52 +0530

    my first program of java

--- day_3.java
+++ day_3.java
@@ -13,6 +13,10 @@ public class day_3 {
 
         PUBLIC == it is the access modifier indicaing that the method can be accesed from the out side of the class .
         STATIC = it help to know the the method is belong to the class rather then an instance of the class
+        VOID ==  specific that the method does not return any value .
+                  main :The name of the method
+
+        String[] args: The method accept an array of the string as parameter . This is where command line arguments can be passed to your program .
 
 
         *
