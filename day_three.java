6dd99e0baceadc6b8655bd3e93142da196e7f239
ritesh <riteshraj.developer@gmail.com>
2025-02-03 22:21:29 +0530

    my first program of java

---
+++ day_three.java
@@ -0,0 +1,23 @@
+public class day_three {
+
+    public static void main(String[] args) {
+        System.out.println();
+
+        /* Here what is CLASS in the java
+
+        (every thing that came in class in the java)
+
+        THEN NOW WHAT IS (PUBLIC STATIC VOID MAIN)
+
+         here is the main is a method , the main method is the entry point of the program
+
+        PUBLIC == it is the
+
+
+        *
+        *
+        *
+        *
+        * */
+    }
+}
