ffaf1fe7a75ee6b50714334b3605ab666e1f3f8b
ritesh <riteshraj.developer@gmail.com>
2025-02-03 22:14:03 +0530

    my first program of java

---
+++ day2.java
@@ -0,0 +1,17 @@
+public class day2 {
+
+    public static void main(String[] args) {
+        System.out.println("WHAT IS JDK, JVM, ,JRE in java ");
+        // In the java the JDK come First then the JRE came and then the JVM came.
+        /*
+        WHAT IS THE FULL FORM OF THE ALL THREE AND THERE EXPLAINATION .
+
+        1st JDK = Java development kit
+        2nd JRE = java runtime enviorment
+        3rd JVM = java virtual machine
+
+
+
+         */
+    }
+}
