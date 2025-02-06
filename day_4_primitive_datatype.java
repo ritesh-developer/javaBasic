0e62c729f4a921df32b5c7b391dd7fa4cf36be5b
ritesh <riteshraj.developer@gmail.com>
2025-02-07 01:11:45 +0530

    what is the primitive data type in the java

---
+++ day_4_primitive_datatype.java
@@ -0,0 +1,17 @@
+public class day_4_primitive_datatype {
+    public static void main(string[] args) {
+        /* what is primitive data type in the java
+
+
+        whole number
+        byte
+        short
+        int
+        long
+
+
+         */
+
+
+    }
+}
