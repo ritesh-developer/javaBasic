709a4116bce33ff18f9d2c7b290e4f37b5c949ae
ritesh <riteshraj.developer@gmail.com>
2025-02-08 12:45:31 +0530

    what is the primitive data type in the java

--- day_4_primitive_datatype.java
+++ day_4_primitive_datatype.java
@@ -13,6 +13,21 @@ public class day_4_primitive_datatype {
      //decimal number
         * float         // se store karte hai //we store the decimal value in the float
         * double        // we use sometime this double for the store the decimal value in the java
+
+     // characters
+        * char          // we use the char for store the character value in the java
+
+     // booleans
+
+        * boolean       // we use the boolean for  store the true or false data in the java ;
+
+
+
+
+
+
+
+
          */
 
 
